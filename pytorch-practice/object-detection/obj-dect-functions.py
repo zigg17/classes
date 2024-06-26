caf61daa556a3e49d01636849a7f3aae31cbe562
zigg17 <jacob.ziegler@colorado.edu>
2024-06-25 20:03:00 -0600

    Add: non-max suppression function

--- pytorch-practice/object-detection/obj-dect-functions.py
+++ pytorch-practice/object-detection/obj-dect-functions.py
@@ -2,7 +2,8 @@ import torch
 
 class corner_box:
     # Corner instantiation
-    def __init__(self, x1: float, x2: float, y1: float, y2: float, class_num: int, probability: float):
+    def __init__(self, x1: float, x2: float, y1: float, y2: float,
+                 class_num: int, probability: float):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
@@ -20,8 +21,9 @@ class corner_box:
         return midpoint_box(x, y, width, height, self.class_num, self.probability)
 
 class midpoint_box:
-    # midpoint and width instantiation
-    def __init__(self, x: float, y: float, width: float, height: float, class_num: int, probability: float):
+    # Midpoint and width instantiation
+    def __init__(self, x: float, y: float, width: float,
+                 height: float, class_num: int, probability: float):
         self.x = x
         self.y = y
         self.width = width
@@ -40,13 +42,13 @@ class midpoint_box:
         return corner_box(x1, y1, x2, y2, self.class_num, self.probability)
 
 def intersection_over_union(box1, box2):
-    # converts non-corner boxes to corner boxes
+    # Converts non-corner boxes to corner boxes
     if (isinstance(box1, midpoint_box)):
         box1 = box1.convert_to_corner()
     if (isinstance(box2, midpoint_box)):
         box2 = box2.convert_to_corner()
     
-    # corner calculations
+    # Corner calculations
     x1 = torch.max(box1.x1, box2.x1)
     y1 = torch.max(box1.y1, box2.y1)
 
@@ -58,3 +60,16 @@ def intersection_over_union(box1, box2):
     union = (abs((box1.y2 - box1.y1) * (box1.x2 - box1.x1)) + abs((box2.y2 - box2.y1) * (box2.x2 - box2.x1))) - intersection
 
     return intersection / union
+
+def non_max_suppression(boxes: list, iou_threshold: float,
+                        probability_threshold: float):
+    for box in boxes:
+        # Convert all possible midpoint boxes to corner boxes
+        if (isinstance(box, midpoint_box)):
+            box = box.convert_to_corner()
+
+        # Removes boxes below a certain probability threshold to reduce complexity
+        if(box.probability < probability_threshold):
+            boxes.remove(box)
+
+    boxes = sorted(boxes, key=lambda box: box.probability, reverse=True)
